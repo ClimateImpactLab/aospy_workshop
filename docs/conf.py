cec41e96840a3b56d54f6ce9728a6750ee5d3dae
jgerardsimcock <justin_simcock@yahoo.com>
2016-11-05 07:29:19 -0700

    update conf.py

--- docs/conf.py
+++ docs/conf.py
@@ -38,10 +38,7 @@ extensions = [
     'sphinx.ext.intersphinx',
     'sphinx.ext.todo',
     'sphinx.ext.coverage',
-    'sphinx.ext.mathjax',
-    'sphinx.ext.ifconfig',
-    'sphinx.ext.viewcode',
-    'sphinx.ext.githubpages',
+    'sphinx.ext.mathjax'
 ]
 
 # Add any paths that contain templates here, relative to this directory.
